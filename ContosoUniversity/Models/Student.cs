79e4738a0a9aed61364aa38b69bc609f8f7dc7c3
Mak <makram3ibrahim@gmail.com>
2018-03-30 18:51:12 -0600

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string StudentLastName { get; set; }
+        public string StudentMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
